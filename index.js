b713ca58311e0914a1afe51a8fac69a52c137314
greenlineenergy <nyashfackedit@gmail.com>
2025-08-20 15:48:11 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>GreenLine Energy</h1>
+      <p>Welcome to our new site — deployed on Vercel 🚀</p>
+    </div>
+  );
+}
